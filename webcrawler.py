7e80d0f65331c9cbb69bcb34d8e6d26852fa6017
guokweijie <guokweijie@gmail.com>
2023-11-07 22:46:04 +0800

    strip url

--- webcrawler.py
+++ webcrawler.py
@@ -39,12 +39,13 @@ def crawl(url, file_path, file_lock, is_last_level):
         soup = BeautifulSoup(response.content, "html.parser")
         wordcount = []
         #page_content = urllib.request.urlopen(url).read().decode('utf-8')
-        if 'scam' not in response.text and 'fraud' not in response.text:
+        page_content = response.text.lower()
+        if 'scam' not in page_content and 'fraud' not in page_content:
             print('irrelevant link - exiting')
             return
         wordcount = []
         for word in keyword:
-            if word in response.text:
+            if word in page_content:
                 wordcount.append('1')
             else:
                 wordcount.append('0')
