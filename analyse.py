719395288399282afbf96ce91a7cd3f42f0d9cbb
emptygx <e0717559@u.nus.edu>
2023-11-09 21:25:30 +0800

    Update analyse.py to new format and count by country

--- analyse.py
+++ analyse.py
@@ -20,7 +20,7 @@ with open('visited.txt', 'r') as file:
         for i in range(10):
             if r not in region_keyword_counts:
                 region_keyword_counts[r] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            region_keyword_counts[r][i] += int(info[i + 4])
+            region_keyword_counts[r][i] += int(info[i + 5])
 
 for region, counts in region_keyword_counts.items():
     print(f'Region: {region}')
