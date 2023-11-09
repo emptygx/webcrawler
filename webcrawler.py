a834487b72e6ca71037a9f330dccb15edac0a3ea
emptygx <e0717559@u.nus.edu>
2023-11-09 21:59:51 +0800

    Minor documentation

--- webcrawler.py
+++ webcrawler.py
@@ -6,6 +6,9 @@ import time
 import socket
 import multiprocessing
 
+# Links will be added to text file links.txt
+# Information from crawling will be added to text file visited.txt
+
 # Keywords for analysis on scams
 keyword = ['phishing',
 'impersonation',
@@ -28,9 +31,12 @@ def crawl(url, file_path, file_lock, is_last_level):
         soup = BeautifulSoup(response.content, "html.parser")
         wordcount = []
         page_content = response.text.lower()
+
+        # Check if link has relevant content before crawling
         if 'scam' not in page_content and 'fraud' not in page_content:
-            print('irrelevant link - exiting')
             return
+        
+        # Substring matching for chosen set of keywords
         wordcount = []
         for word in keyword:
             if word in page_content:
@@ -40,10 +46,16 @@ def crawl(url, file_path, file_lock, is_last_level):
 
         # Getting information (response time, country, region) associated to current url
         response_time = response.elapsed.total_seconds()
+
+        # Get IP Address
         ip = socket.gethostbyname(response.url.split('//')[1].split('/')[0])
+
+        # Get region data from ip-api.com API
         ip_api_res = requests.get(f'http://ip-api.com/json/{ip}').json()
+        # If rate limit exceeded, print the failed response message
         if ip_api_res.get("status") == "fail":
             print(ip_api_res.get("message"))
+        # Get country and region names for appending
         country = ip_api_res.get("country")
         region = ip_api_res.get("regionName")
         if region == None:
@@ -51,7 +63,7 @@ def crawl(url, file_path, file_lock, is_last_level):
         if country == None:
             country = "Unknown"
 
-        # Parse html for unique urls if not at max depth yet
+        # Parse html for unique urls to add if not at max depth yet
         links = []
         if not is_last_level:
             links = [a["href"] for a in soup.find_all(
@@ -59,10 +71,13 @@ def crawl(url, file_path, file_lock, is_last_level):
 
             links = list(set(links))
 
-
         new_links = []
+
         # Append information to the text files with file lock
+        # Access-lock mechanism to coordinate the prcoesses and ensure mutex when writing to the file, 
+        # preventing duplicate links as well
         with file_lock:
+            
             # Filter out duplicates and already visited URLs
             with open(file_path, "r") as file:
                 existing_links = file.readlines()
@@ -79,13 +94,14 @@ def crawl(url, file_path, file_lock, is_last_level):
                 print(line)
                 file.write(line + "\n")
 
-            # Add new links to links.txt which contains all the urls to parse
+            # Add new links to links.txt which contains all the unique found urls thus far
             with open(file_path, "a") as file:
                 for link in new_links:
                     file.write(link + "\n")
 
         return new_links
 
+    # Catches exceptions that occur while crawling a url
     except Exception as e:
         print(f"Error crawling {url}: {e}")
         return []
@@ -108,9 +124,10 @@ def main(num_workers=12, max_depth=2):
             title = title + "|" + word
         file.write(title + "\n")
 
-    # Pointer to keep track of oldest uncrawled link position
+    # Pointer to keep track of oldest uncrawled link position in links.txt
     pointer = 0
 
+    # Initialise depth start the loop
     depth = 1
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
         while depth <= max_depth:
@@ -118,20 +135,26 @@ def main(num_workers=12, max_depth=2):
             # Crawl initial set of urls, then batch of urls returned from previous depth subsequently
             print(f"Processing depth {depth}...")
             start_depth_time = time.time()
+            
+            # Save the current eof as the next pointer to use
             next_pointer = len(open(file_path).readlines())
+
+            # Set the current batch of urls to be those from the previous iteration onwards
             urls = open(file_path).readlines()[pointer:]
             
             # Create a list to store the Future objects
             futures = []
             
             for url in urls:
-                # Submit the task to the executor
+                # Executor launches a process with one of the urls to crawl
                 future = executor.submit(crawl, url, file_path, file_lock, depth == max_depth)
                 futures.append(future)
                 
-                # Introduce a delay before assigning the next task
+                # Introduce a delay before launching the next process,
+                # so as not to exceed the geolocation API rate limit
                 time.sleep(2)
             
+            # Move pointer to the last crawled url
             pointer = next_pointer
 
             # Wait for all current depth's urls to be crawled
@@ -162,4 +185,6 @@ if __name__ == "__main__":
             file.write(start_url + "\n")
     
     # Run main function to start crawling
+    # Set num_workers to max number of processes to launch
+    # Set max_depth to how many layers of added links to crawl from the initial set of urls
     main(num_workers=5, max_depth=10)
